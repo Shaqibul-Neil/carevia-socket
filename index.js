e4043b05eb83e47edf59a6c04c73fdc35e1ecb3c
shaqibul-neil <shaqibul.islam.neil@gmail.com>
2026-02-13 06:17:03 +0600

    socket events logic added and created multiple events

--- index.js
+++ index.js
@@ -11,9 +11,32 @@ const server = http.createServer(app); //http server
 
 const io = new Server(server, {
   cors: {
-    origin: "*", //allowed all anyone can send message
+    origin: "*", //allowed to all anyone can send message
     methods: ["GET", "POST"], //method allowed
   },
+});
+
+io.on("connection", (socket) => {
+  console.log("user connected", socket.id);
+
+  //events
+  socket.on("join_room", (room) => {
+    socket.join(room);
+    console.log(`User ${socket.id} joined ${room}`);
+  });
+  //data has the message and the receiver information
+  socket.on("send_message", (data) => {
+    //getting the info of room from data then broadcasts the same event with a new event and same message
+    socket.to(data.room).emit("receive_message", data);
+  });
+
+  socket.on("typing", ({ userName, room }) => {
+    socket.to(room).emit("user_typing", userName);
+  });
+
+  socket.on("disconnect", () => {
+    console.log(`User disconnected : `, socket.id);
+  });
 });
 
 server.listen(3001, () => {
