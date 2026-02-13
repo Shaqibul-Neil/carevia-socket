f78b2add227e10ac3695d4d6ace98fa2931b3695
shaqibul-neil <shaqibul.islam.neil@gmail.com>
2026-02-14 05:48:35 +0600

    craeted http route for braodcast booking event

--- index.js
+++ index.js
@@ -23,17 +23,23 @@ const io = new Server(server, {
 
 io.on("connection", (socket) => {
   console.log("user connected", socket.id);
-
   socket.on("join_room", (room) => {
     socket.join(room);
     console.log(`User ${socket.id} joined ${room}`);
   });
+
+  // Listen for new bookings from Next.js
+  socket.on("new_booking", (customerData) => {
+    console.log("New booking:", customerData);
+    // Broadcast to all connected dashboards
+    io.emit("customer_added", customerData);
+  });
+
   //data has the message and the receiver information
   socket.on("send_message", (data) => {
     //getting the info of room from data then broadcasts the same event with a new event and same message
     console.log("Got Message", data);
     socket.to(data.room).emit("receive_message", data);
-
     // Broadcast to EVERYONE in the room (including sender)
     io.to(data.roomId).emit("receive_message", data);
   });
@@ -50,6 +56,26 @@ io.on("connection", (socket) => {
 // ==========================================
 // HTTP API Endpoints (for Next.js to call)
 // ==========================================
+app.post("/emit-booking", async (req, res) => {
+  try {
+    const customerData = req.body;
+    if (!customerData.userId) {
+      return res
+        .status(500)
+        .send({ success: false, message: "Invalid id required" });
+    }
+    // Step 3: Log for debugging
+    console.log("📢 Broadcasting new customer:", customerData);
+    io.emit("customer_added", customerData);
+    return res.json({
+      success: true,
+      message: "Broadcast Successful",
+      clientCount: io.engine.clientsCount,
+    });
+  } catch (error) {
+    return res.status(500).json({ error: error.messgae });
+  }
+});
 
 server.listen(3001, () => {
   console.log("Server listening on port 3001");
